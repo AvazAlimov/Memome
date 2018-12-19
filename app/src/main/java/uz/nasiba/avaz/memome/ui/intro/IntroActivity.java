6e5c8d12004f4d35958735fd4efd3093f754e558
AvazAlimov <avazalimov95@mail.ru>
2018-12-20 00:06:05 +0500

    Intro Activity changed

--- app/src/main/java/uz/nasiba/avaz/memome/ui/intro/IntroActivity.java
+++ app/src/main/java/uz/nasiba/avaz/memome/ui/intro/IntroActivity.java
@@ -2,12 +2,11 @@ package uz.nasiba.avaz.memome.ui.intro;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 import java.util.ArrayList;
 
@@ -15,71 +14,77 @@ import uz.nasiba.avaz.memome.R;
 import uz.nasiba.avaz.memome.ui.auth.AuthActivity;
 
 public class IntroActivity extends AppCompatActivity {
+    private int index = 0;
+    private ArrayList<Integer> animations;
+    private ArrayList<Integer> titles;
+    private ArrayList<Integer> texts;
+    private LottieAnimationView animationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
-
-        ViewPager pager = findViewById(R.id.pager);
-        PagerAdapter adapter = new SlidePagerAdapter(getSupportFragmentManager());
-        pager.setAdapter(adapter);
+        initialize();
+        update();
     }
 
-    @Override
-    public void finishActivity(int requestCode) {
-        if (requestCode == 0) {
-            Intent intent = new Intent(this, AuthActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
+    private void initialize() {
+        animations = new ArrayList<>();
+        animations.add(R.raw.intro_one);
+        animations.add(R.raw.intro_two);
+        animations.add(R.raw.intro_three);
+
+        titles = new ArrayList<>();
+        titles.add(R.string.intro_one);
+        titles.add(R.string.intro_two);
+        titles.add(R.string.intro_three);
+
+        texts = new ArrayList<>();
+        texts.add(R.string.intro_one_text);
+        texts.add(R.string.intro_two_text);
+        texts.add(R.string.intro_three_text);
+
+        animationView = findViewById(R.id.animator);
+    }
+
+    private void update() {
+        animationView.setAnimation(animations.get(index));
+        animationView.resumeAnimation();
+
+        ((TextView) findViewById(R.id.title)).setText(titles.get(index));
+        ((TextView) findViewById(R.id.text)).setText(texts.get(index));
+        if (index == 0) {
+            findViewById(R.id.previous).setVisibility(View.GONE);
+            findViewById(R.id.next).setVisibility(View.VISIBLE);
+            findViewById(R.id.finish).setVisibility(View.GONE);
+        } else if (index == animations.size() - 1) {
+            findViewById(R.id.previous).setVisibility(View.VISIBLE);
+            findViewById(R.id.next).setVisibility(View.GONE);
+            findViewById(R.id.finish).setVisibility(View.VISIBLE);
+        } else {
+            findViewById(R.id.previous).setVisibility(View.VISIBLE);
+            findViewById(R.id.next).setVisibility(View.VISIBLE);
+            findViewById(R.id.finish).setVisibility(View.GONE);
         }
     }
 
-    private class SlidePagerAdapter extends FragmentStatePagerAdapter {
-        private ArrayList<Integer> animations;
-        private ArrayList<Integer> colors;
-        private ArrayList<Integer> titles;
-        private ArrayList<Integer> texts;
-
-        SlidePagerAdapter(FragmentManager fragmentManager) {
-            super(fragmentManager);
-
-            animations = new ArrayList<>();
-            animations.add(R.raw.intro_one);
-            animations.add(R.raw.intro_two);
-            animations.add(R.raw.intro_three);
-
-            colors = new ArrayList<>();
-            colors.add(0xFFFFF9C4);
-            colors.add(0xFFC8E6C9);
-            colors.add(0xFFB3E5FC);
-
-            titles = new ArrayList<>();
-            titles.add(R.string.intro_one);
-            titles.add(R.string.intro_two);
-            titles.add(R.string.intro_three);
-
-            texts = new ArrayList<>();
-            texts.add(R.string.intro_one_text);
-            texts.add(R.string.intro_two_text);
-            texts.add(R.string.intro_three_text);
+    public void next(View view) {
+        if (index < animations.size() - 1) {
+            index++;
         }
+        update();
+    }
 
-        @Override
-        public Fragment getItem(int position) {
-            IntroFragment fragment = new IntroFragment();
-            Bundle arguments = new Bundle();
-            arguments.putInt("animation", animations.get(position));
-            arguments.putInt("color", colors.get(position));
-            arguments.putInt("title", titles.get(position));
-            arguments.putInt("text", texts.get(position));
-            arguments.putBoolean("isFinish", position == animations.size() - 1);
-            fragment.setArguments(arguments);
-            return fragment;
+    public void previous(View view) {
+        if (index > 0) {
+            index--;
         }
+        update();
+    }
 
-        @Override
-        public int getCount() {
-            return animations.size();
-        }
+    public void finish(View view) {
+        Intent intent = new Intent(this, AuthActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
