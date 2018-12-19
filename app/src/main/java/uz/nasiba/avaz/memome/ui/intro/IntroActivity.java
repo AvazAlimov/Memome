59a65041c10c6ab8f9b8e346f94dfc3ab5038df6
AvazAlimov <avazalimov95@mail.ru>
2018-12-19 18:37:16 +0500

    Utils added: - Activity with fragment added - LocaleManager modified Firebase auth added

--- app/src/main/java/uz/nasiba/avaz/memome/ui/intro/IntroActivity.java
+++ app/src/main/java/uz/nasiba/avaz/memome/ui/intro/IntroActivity.java
@@ -1,12 +1,12 @@
 package uz.nasiba.avaz.memome.ui.intro;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 import java.util.ArrayList;
 
@@ -16,6 +16,7 @@ public class IntroActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_intro);
 
         ViewPager pager = findViewById(R.id.pager);
